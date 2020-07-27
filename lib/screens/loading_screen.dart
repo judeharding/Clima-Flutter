6d66bece2dfe4bd4dd686f07f37ce7e0726906b3
judeharding <jharding@genesisgroupusa.com>
2020-07-27 17:28:43 -0400

    WIP sectin 13

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,16 +8,31 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  // async and await go hand in hand - goes on in the background
+  void getLocation() async {
+    Position position = await Geolocator()
+        // the more "accurate" you are below, the more battery you will use
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+//    super.getLocation();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+//        child: RaisedButton(
+//          onPressed: () {
+//            getLocation();
+//          },
+//          child: Text('Get Location'),
+//        ),
+          ),
     );
   }
 }
